271a958c7e944e2dbf17ee85af1c024e86a4fb21
오동규 <ehdrb700606@naver.com>
2024-07-29 23:45:03 +0900

    [feat](BE): setting 조회, 수정 기능 추가

--- BE/mvp/src/main/java/com/mvp/parkinglot/controller/ParkingLotController.java
+++ BE/mvp/src/main/java/com/mvp/parkinglot/controller/ParkingLotController.java
@@ -1,11 +1,13 @@
 package com.mvp.parkinglot.controller;
 
 import com.mvp.parkinglot.dto.ParkingLotMapDTO;
+import com.mvp.parkinglot.dto.ParkingLotSettingDTO;
 import com.mvp.parkinglot.service.ParkingLotService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.function.EntityResponse;
 
 @RestController
@@ -17,8 +19,22 @@ public class ParkingLotController {
 
     @GetMapping("/map")
     public ParkingLotMapDTO getMap(){
+        // 공통 예외처리 필요
         return parkingLotService.getMap();
     }
 
     @GetMapping("/setting")
+    public ParkingLotSettingDTO getSetting(){
+        // 공통 예외처리 필요
+        return parkingLotService.getSetting();
+    }
+
+    @PutMapping("/setting")
+    public ResponseEntity<?> updateSetting(@RequestBody ParkingLotSettingDTO parkingLotSettingDTO){
+        // 공통 예외처리 필요
+        parkingLotService.updateSetting(parkingLotSettingDTO);
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+
 }
