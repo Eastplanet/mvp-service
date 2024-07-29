1798796001b6783e30832fa776f9317e67fd0986
손원륜 <wrson729@gmail.com>
2024-07-29 15:58:46 +0900

    [Feat] (FE) 설정화면 구현 중

--- FE/mvp/src/App.tsx
+++ FE/mvp/src/App.tsx
@@ -1,23 +1,32 @@
 // src/App.tsx
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from './store/store';
 import Login from './features/auth/Login';
-import Main from './features/main/Main'; // 예: 로그인 후 보여질 대시보드 컴포넌트
+import Main from './features/main/Main';
+import Set from './features/set/Set';
 import './App.css';
 
 function App() {
-  // Redux 상태에서 인증 상태를 가져옵니다.
   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
 
   return (
-    <div className="App">
-      {isAuthenticated ? (
-        <Main /> // 사용자가 로그인된 경우, 대시보드 컴포넌트를 렌더링
-      ) : (
-        <Login /> // 로그인되지 않은 경우, 로그인 컴포넌트를 렌더링
-      )}
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {isAuthenticated ? (
+            <>
+              <Route path="/home" element={<Main />} />
+              <Route path="/settings" element={<Set />} />
+              {/* 여기에 추가적인 라우트를 설정할 수 있습니다. */}
+            </>
+          ) : (
+            <Route path="/login" element={<Login />} />
+          )}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
