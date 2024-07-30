7381eca4ea4cccab331417874b67e8d3ec2ccb56
김세진 <tpwls1355@naver.com>
2024-07-30 21:27:06 +0900

    [Feat] (BE) : ParkedVehicle Get API
    주차 차량 조회 API

--- BE/mvp/src/main/java/com/mvp/vehicle/controller/VehicleController.java
+++ BE/mvp/src/main/java/com/mvp/vehicle/controller/VehicleController.java
@@ -1,4 +1,40 @@
 package com.mvp.vehicle.controller;
 
+import com.mvp.vehicle.entity.ParkedVehicle;
+import com.mvp.vehicle.service.VehicleService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/parked-vehicle")
+@AllArgsConstructor
 public class VehicleController {
+    private final VehicleService vehicleService;
+
+    @GetMapping("/{vehicleId}")
+    public ResponseEntity<ParkedVehicle> getParkedVehicle(@PathVariable Long vehicleId){
+        ParkedVehicle parkedVehicle = vehicleService.getParkedVehicle(vehicleId);
+        if(parkedVehicle != null){
+            return ResponseEntity.ok(parkedVehicle);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<ParkedVehicle>> getParkedVehicleList(){
+        List<ParkedVehicle> parkedVehicleList = vehicleService.getParkedVehicleList();
+
+        if(!parkedVehicleList.isEmpty()){
+            return ResponseEntity.ok(parkedVehicleList);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- BE/mvp/src/main/java/com/mvp/vehicle/service/VehicleService.java
+++ BE/mvp/src/main/java/com/mvp/vehicle/service/VehicleService.java
@@ -1,4 +1,27 @@
 package com.mvp.vehicle.service;
 
+import com.mvp.vehicle.entity.ParkedVehicle;
+import com.mvp.vehicle.repository.ParkedVehicleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class VehicleService {
+    private final ParkedVehicleRepository parkedVehicleRepository;
+
+    public ParkedVehicle getParkedVehicle(Long vehicleId) {
+        ParkedVehicle parkedVehicle = parkedVehicleRepository.findById(vehicleId).orElse(null);
+        if(parkedVehicle != null){
+            return parkedVehicle;
+        } else {
+            return null;
+        }
+    }
+
+    public List<ParkedVehicle> getParkedVehicleList() {
+        return parkedVehicleRepository.findAll();
+    }
 }
