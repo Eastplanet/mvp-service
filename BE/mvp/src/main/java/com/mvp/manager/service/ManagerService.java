5cbe8978cfac4d3ab9c242865e4c2961eae366c3
오동규 <ehdrb700606@naver.com>
2024-07-31 15:02:14 +0900

    [chore](BE) : 컨벤션 준수

--- BE/mvp/src/main/java/com/mvp/manager/service/ManagerService.java
+++ BE/mvp/src/main/java/com/mvp/manager/service/ManagerService.java
@@ -7,14 +7,10 @@ import com.mvp.manager.converter.ManagerConverter;
 import com.mvp.manager.dto.ManagerDTO;
 import com.mvp.manager.entity.Manager;
 import com.mvp.manager.repository.ManagerRepository;
-import com.mvp.parkinglot.dto.ParkingLotDTO;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,7 +19,9 @@ public class ManagerService {
     private final ManagerRepository managerRepository;
 
     public ManagerDTO createManager(ManagerDTO managerDTO) {
-        if(checkDuplicateEmail(managerDTO)) throw new RestApiException(StatusCode.DUPLICATE_EMAIL);
+        if(checkDuplicateEmail(managerDTO)) {
+            throw new RestApiException(StatusCode.DUPLICATE_EMAIL);
+        }
         managerDTO.setPassword(HashUtil.sha256(managerDTO.getPassword()));
         Manager manager = ManagerConverter.dtoToEntity(managerDTO);
         Manager save = managerRepository.save(manager);
@@ -32,7 +30,9 @@ public class ManagerService {
 
     public ManagerDTO login(ManagerDTO managerDTO) {
         Optional<Manager> find = managerRepository.findByEmail(managerDTO.getEmail());
-        if(find.isEmpty()) throw new RestApiException(StatusCode.INVALID_EMAIL_OR_PASSWORD);
+        if(find.isEmpty()) {
+            throw new RestApiException(StatusCode.INVALID_EMAIL_OR_PASSWORD);
+        }
         Manager manager = find.get();
 
         if(manager.getPassword().equals(HashUtil.sha256(managerDTO.getPassword()))) {
@@ -45,14 +45,18 @@ public class ManagerService {
 
     public ManagerDTO updateManager(ManagerDTO managerDTO) {
         Optional<Manager> find = managerRepository.findByEmail(managerDTO.getEmail());
-        if(find.isEmpty()) throw new RestApiException(StatusCode.NO_SUCH_ELEMENT);
+        if(find.isEmpty()) {
+            throw new RestApiException(StatusCode.NO_SUCH_ELEMENT);
+        }
         managerDTO.setPassword(HashUtil.sha256(managerDTO.getPassword()));
 
         Manager manager = find.get();
         manager.update(managerDTO);
         Manager result = managerRepository.save(manager);
 
-        if(result == null) throw new RestApiException(StatusCode.INTERNAL_SERVER_ERROR);
+        if(result == null) {
+            throw new RestApiException(StatusCode.INTERNAL_SERVER_ERROR);
+        }
         return ManagerConverter.entityToDto(result);
     }
 
