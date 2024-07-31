a17f959d1417a8d020f9681ba5ddbf4c14c697ea
손원륜 <wrson729@gmail.com>
2024-07-31 16:09:08 +0900

    [Wip](FE) 차트페이지 수정 중

--- FE/mvp/src/features/chart/Chart.tsx
+++ FE/mvp/src/features/chart/Chart.tsx
@@ -1,28 +1,4 @@
-// import React from 'react';
-// import Sidebar from '../sidebar/Sidebar';
-// import styles from './Set.module.css';
 
-// const Set: React.FC = () => {
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.sidebarWrapper}>
-//         <Sidebar />
-//       </div>
-//       <div className={styles.page}>
-//         <div className={styles.runtime}>
-//           <p className={styles.title}>관리 운영 시간</p>
-//           <p>주일</p>
-//           <div className={styles.timeInputs}>
-//             <input type="time" className={styles.timeInput} /> ~
-//             <input type="time" className={styles.timeInput} />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Set;
 
 import React from 'react';
 import Sidebar from '../sidebar/Sidebar';
@@ -36,23 +12,65 @@ const Set: React.FC = () => {
         <p className={styles.title}>Chart</p>
         <div className={styles.datas}>
           <div className={styles.data}>
-            <div>
+            <div className={styles.data_left}>
               <span>이번 달 매출</span>
-              <span>회원 제외</span>
+              <span className={styles.small_font}>회원 제외</span>
             </div>
-            <div>
-              <span>금액</span>
-              <span>원</span>
+            <div className={styles.data_right}>
+              <span>1000000</span>
+              <span>(천)원</span>
             </div>
-            <div className={styles.dif}>
-              <img src="" alt="" />
-              <span>계산값%</span> 
-              <span>지난 달 대비</span>
+            <div className={styles.data_left}>
+              <img src="" alt="화살표" />
+              <span>1%</span> 
+              <span className={styles.small_font}>지난 달 대비</span>
             </div>
           </div>
-          <div className={styles.data}>22</div>
-          <div className={styles.data}>33</div>
-          <div className={styles.data}>44</div>
+          <div className={styles.data}>
+          <div className={styles.data_left}>
+              <span>이번 달 주차량</span>
+              <span className={styles.small_font}>회원 제외</span>
+            </div>
+            <div className={styles.data_right}>
+              <span>1000</span>
+              <span>대</span>
+            </div>
+            <div className={styles.data_left}>
+              <img src="" alt="화살표" />
+              <span>1%</span> 
+              <span className={styles.small_font}>지난 달 대비</span>
+            </div>
+          </div>
+          <div className={styles.data}>
+          <div className={styles.data_left}>
+              <span>오늘 매출</span>
+              <span className={styles.small_font}>회원 제외</span>
+            </div>
+            <div className={styles.data_right}>
+              <span>1000000</span>
+              <span>(천)원</span>
+            </div>
+            <div className={styles.data_left}>
+              <img src="" alt="화살표" />
+              <span>1%</span> 
+              <span className={styles.small_font}>지난 달 대비</span>
+            </div>
+          </div>
+          <div className={styles.data}>
+          <div className={styles.data_left}>
+              <span>오늘 주차량</span>
+              <span className={styles.small_font}>회원 제외</span>
+            </div>
+            <div className={styles.data_right}>
+              <span>100</span>
+              <span>대</span>
+            </div>
+            <div className={styles.data_left}>
+              <img src="" alt="화살표" />
+              <span>1%</span> 
+              <span className={styles.small_font}>지난 달 대비</span>
+            </div>
+          </div>
         </div>
         <div className={styles.chart}>
           가운데 아랫 부분
--- FE/mvp/src/features/set/Set.tsx
+++ FE/mvp/src/features/set/Set.tsx
@@ -1,29 +1,3 @@
-// import React from 'react';
-// import Sidebar from '../sidebar/Sidebar';
-// import styles from './Set.module.css';
-
-// const Set: React.FC = () => {
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.sidebarWrapper}>
-//         <Sidebar />
-//       </div>
-//       <div className={styles.page}>
-//         <div className={styles.runtime}>
-//           <p className={styles.title}>관리 운영 시간</p>
-//           <p>주일</p>
-//           <div className={styles.timeInputs}>
-//             <input type="time" className={styles.timeInput} /> ~
-//             <input type="time" className={styles.timeInput} />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Set;
-
 import React from 'react';
 import Sidebar from '../sidebar/Sidebar';
 import styles from './Set.module.css';
