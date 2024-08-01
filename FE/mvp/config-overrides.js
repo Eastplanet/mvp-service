3e35d4daf5fc7ce0249bf3a6987e7969c8088ec6
조성인 <sijo0209@naver.com>
2024-08-01 16:18:57 +0900

    [wip] (FE) api 정상화 중

--- FE/mvp/config-overrides.js
+++ FE/mvp/config-overrides.js
@@ -1,14 +1,10 @@
 // config-overrides.js
 module.exports = function override(config, env) {
-    console.log('Overriding devServer configuration'); // 로그 추가
+    console.log('Overriding devServer configuration');
     config.devServer = {
       ...config.devServer,
       host: 'mvp-project.shop',
       port: 8081,
-      allowedHosts: [
-        'mvp-project.shop'
-      ],
-      disableHostCheck: true,
     };
     console.log('-------------------------------------------------');
     console.log('devServer configuration:', config.devServer);
