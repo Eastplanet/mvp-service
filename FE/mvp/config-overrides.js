7c3923ee02305e780d4402e3e573e718464a09d7
조성인 <sijo0209@naver.com>
2024-08-01 12:13:06 +0900

    [fix] (FE) host error 수정

--- FE/mvp/config-overrides.js
+++ FE/mvp/config-overrides.js
@@ -1,12 +1,10 @@
 // config-overrides.js
 module.exports = function override(config, env) {
-    if (env === 'development') {
-      console.log('Overriding devServer configuration'); // 로그 추가
-      config.devServer = {
-        ...config.devServer,
-        disableHostCheck: true, // 호스트 검사를 비활성화
-      };
-    }
+    console.log('Overriding devServer configuration'); // 로그 추가
+    config.devServer = {
+      ...config.devServer,
+      disableHostCheck: true,
+    };
     return config;
   };
   
