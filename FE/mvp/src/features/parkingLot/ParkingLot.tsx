79e5e5a4f0c13030bb449777139e3e8e7fe62810
조성인 <sijo0209@naver.com>
2024-08-01 09:36:38 +0900

    [feat] (FE) 주차장 컴포넌트 구현

--- FE/mvp/src/features/parkingLot/ParkingLot.tsx
+++ FE/mvp/src/features/parkingLot/ParkingLot.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './ParkingLot.module.css';
-import parkingCar from '../../assets/images/icons/parking_car.png'
+import parkingCar from '../../assets/images/icons/parking_car.png';
 
 type CarLog = {
   carNumber: string;
@@ -21,24 +21,24 @@ const ParkingLot: React.FC<ParkingLotProps> = ({ parkingData, onCarLogClick }) =
   return (
     <div className={styles.parkingLotContainer}>
       <div className={styles.parkingLot}>
-        <div className={styles.side}>
+        <div className={`${styles.side} ${styles.leftSide}`}>
           {parkingData.slice(0, 3).map((carLog, index) => (
             <div
               key={index}
-              className={`${styles.parkingSpace} ${carLog.carNumber ? styles.occupied : styles.empty}`}
+              className={styles.parkingSpace}
               onClick={() => onCarLogClick(carLog)}
             >
-              {carLog.carNumber && <img src={parkingCar} alt={carLog.carNumber} />}
+              {carLog.carNumber && <img src={parkingCar} alt={carLog.carNumber} className={styles.rotatedCar} />}
             </div>
           ))}
-          <div className={styles.emptySpace}></div>
+          <div className={`${styles.parkingSpace} ${styles.emptySpace}`}></div>
         </div>
         <div className={styles.road}></div>
-        <div className={styles.side}>
+        <div className={`${styles.side} ${styles.rightSide}`}>
           {parkingData.slice(3, 7).map((carLog, index) => (
             <div
               key={index}
-              className={`${styles.parkingSpace} ${carLog.carNumber ? styles.occupied : styles.empty}`}
+              className={styles.parkingSpace}
               onClick={() => onCarLogClick(carLog)}
             >
               {carLog.carNumber && <img src={parkingCar} alt={carLog.carNumber} />}
