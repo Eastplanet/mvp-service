ae21a56c6c9e50d00b20a425af4144336728f6d4
조성인 <sijo0209@naver.com>
2024-08-01 14:52:49 +0900

    [fix] (FE) host 오류 수정

--- FE/mvp/config-overrides.js
+++ FE/mvp/config-overrides.js
@@ -3,8 +3,12 @@ module.exports = function override(config, env) {
     console.log('Overriding devServer configuration'); // 로그 추가
     config.devServer = {
       ...config.devServer,
+      host: 'mvp-project.shop', // 호스트 이름
+      port: 8081,               // 포트 번호
+      allowedHosts: [
+        'mvp-project.shop'      // 허용된 호스트에 도메인 추가
+      ],
       disableHostCheck: true,
-      allowedHosts: 'all',
     };
     console.log('-------------------------------------------------');
     console.log('devServer configuration:', config.devServer);
