75f514b971fec0da24ab9c81b3749f12e234b7ea
오동규 <ehdrb700606@naver.com>
2024-08-01 12:43:34 +0900

    [fix](BE): fix

--- BE/mvp/src/main/java/com/mvp/config/WebConfig.java
+++ BE/mvp/src/main/java/com/mvp/config/WebConfig.java
@@ -5,13 +5,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://www.mvp-project.shop","http://www.mvp-project.shop","http://www.mvp-project.shop:3000","http://localhost:3000","https://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                .allowedHeaders("Authorization", "Content-Type")
-                .allowCredentials(true);
-    }
-}
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("https://www.mvp-project.shop","http://www.mvp-project.shop","http://www.mvp-project.shop:3000","http://localhost:3000","https://localhost:3000")
+//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+//                .allowedHeaders("Authorization", "Content-Type")
+//                .allowCredentials(true);
+//    }
+//}
