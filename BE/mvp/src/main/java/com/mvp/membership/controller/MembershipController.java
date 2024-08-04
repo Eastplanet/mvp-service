8c8c6d510854ee62ae3aff811e7ab630f30d27cf
오동규 <ehdrb700606@naver.com>
2024-08-04 16:38:27 +0900

    [FIX] (BE) : /membership 매핑 -> /memberships 로 변경

--- BE/mvp/src/main/java/com/mvp/membership/controller/MembershipController.java
+++ BE/mvp/src/main/java/com/mvp/membership/controller/MembershipController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/membership")
+@RequestMapping("/api/memberships")
 @AllArgsConstructor
 public class MembershipController {
     private final MembershipService membershipService;
