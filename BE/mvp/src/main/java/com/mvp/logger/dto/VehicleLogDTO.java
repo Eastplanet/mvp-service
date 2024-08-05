ec8ae5ad530a3b05239586eb51c286533a918625
오동규 <ehdrb700606@naver.com>
2024-08-05 23:27:20 +0900

    [Fix](BE) : stats API 오류 수정

--- BE/mvp/src/main/java/com/mvp/logger/dto/VehicleLogDTO.java
+++ BE/mvp/src/main/java/com/mvp/logger/dto/VehicleLogDTO.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class VehicleLogDTO {
     private String licensePlate;
     private LocalDateTime entranceTime;
