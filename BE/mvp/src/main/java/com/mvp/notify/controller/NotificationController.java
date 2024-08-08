cf833b7d2236b70837feb521565de9d69afd3291
오동규 <ehdrb700606@naver.com>
2024-08-08 12:25:26 +0900

    (Fix)[BE] : 응답 형식 수정

--- BE/mvp/src/main/java/com/mvp/notify/controller/NotificationController.java
+++ BE/mvp/src/main/java/com/mvp/notify/controller/NotificationController.java
@@ -1,9 +1,12 @@
 package com.mvp.notify.controller;
 
+import com.mvp.common.ResponseDto;
+import com.mvp.common.exception.StatusCode;
 import com.mvp.notify.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -16,8 +19,8 @@ public class NotificationController {
     private final NotificationService notificationService;
 
     @GetMapping(value = "/subscribe/{email}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(@PathVariable String email, @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId){
-        return notificationService.subscribe(email, lastEventId);
+    public ResponseEntity<?> subscribe(@PathVariable String email, @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId){
+        return ResponseDto.response(StatusCode.SUCCESS,notificationService.subscribe(email, lastEventId));
     }
 
 }
