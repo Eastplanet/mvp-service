30dfede1c04319ffc1f6ae683a7a0f82916f2a64
rt21hyuk <rt21hyuk@gmail.com>
2024-08-09 17:37:36 +0900

    [feat] (EM) : 오린카 장애물 탐지

--- Emberdded/orin/src/bot/getLidarData.py
+++ Emberdded/orin/src/bot/getLidarData.py
@@ -1,11 +1,14 @@
 # getLidarData.py
 
+import numpy as np
 import websocket
 import json
+import time
 import math
 from multiprocessing import Queue
 
 def on_message(ws, message, queue):
+    data = json.loads(message)
     data = json.loads(message)
     try:
         scan_data = data['msg']
@@ -14,29 +17,31 @@ def on_message(ws, message, queue):
         ranges = scan_data['ranges']
         num_ranges = len(ranges)
 
-        segment_angle_size = 0.5
-        num_segments = int(360 / segment_angle_size)
-        segment_max_values = []
+        # 라디안을 도 단위로 변환
+        angle_increment_deg = angle_increment * (180 / math.pi)
+        segment_angle_size_deg = angle_increment_deg * 9
+        num_segments = 360.0 / segment_angle_size_deg  # 2*pi 라디안을 360도로 변환
+        segment_median_values = []
         segment_angles = []
 
-        for i in range(num_segments):
+        for i in range(int(num_segments)):
             segment_start_index = int(i * (num_ranges / num_segments))
             segment_end_index = int((i + 1) * (num_ranges / num_segments))
             segment_values = ranges[segment_start_index:segment_end_index]
             segment_values = [value for value in segment_values if value is not None]
             if segment_values:
-                max_value = max(segment_values)
-                segment_max_values.append(max_value)
-                segment_angles.append((i * segment_angle_size, (i + 1) * segment_angle_size))
+                average_value = np.average(segment_values)
+                segment_median_values.append(average_value)
+                segment_angles.append((i * segment_angle_size_deg, (i + 1) * segment_angle_size_deg))
 
-        if segment_max_values:
-            min_of_max_values = min(segment_max_values)
-            min_index = segment_max_values.index(min_of_max_values)
+        if segment_median_values:
+            min_value = min(segment_median_values)
+            min_index = segment_median_values.index(min_value)
             min_angle_range = segment_angles[min_index]
-            # print(min_of_max_values)
-            queue.put(min_of_max_values)
+            queue.put(min_value)
+            # print(f'minDist : {min_value:.2f}m | {min_angle_range[0]:.2f}~{min_angle_range[1]:.2f}deg')
         else:
-            print('No valid distance found in any segment')
+            print('No valid distance found in any segment')     
         
     except KeyError as e:
         print(f"KeyError: {e}")
@@ -47,7 +52,7 @@ def on_error(ws, error):
     print(f"Error: {error}")
 
 def on_close(ws, close_status_code, close_msg):
-    print("LiDAR Connection closed!")
+    print(f'\033[1;33m[Warning]\033[0m LiDAR Connection closed!')
 
 def on_open(ws):
     print("LiDAR Connection opened!")
