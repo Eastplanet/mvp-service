b5513aab3f370ba9115d62cc8e1f728d2ba703f6
오동규 <ehdrb700606@naver.com>
2024-08-16 11:02:15 +0900

    [feat](Master) : 포팅 메뉴얼

---
+++ exec/프로퍼티 파일 목록/config(라즈베리).py
@@ -0,0 +1,22 @@
+# config/config.py
+RABBIT_MQ_HOST = "mvp-project.shop"
+RABBIT_Q_NAME = "task_queue"
+MQTT_PORT = 1883
+MQTT_BROKER_IP = "localhost"
+MQTT_TOPIC_SUB = "/parking/complete"
+MQTT_TOPIC_PUB = "/parking/command"
+MQTT_BROKER_PATH_WIN = "C:\\Program Files\\mosquitto\\mosquitto.exe"
+MQTT_CONF_PATH_WIN = "C:\\Program Files\\mosquitto\\mosquitto.conf"
+MQTT_BROKER_PATH_RASP = "/usr/sbin/mosquitto"
+MQTT_CONF_PATH_RASP = "/etc/mosquitto/conf.d/external_access.conf"
+SERVER_URL = "https://mvp-project.shop/api"
+
+API_KEY_LPR = "l7xx846db5f3bc1e48d29b7275a745d501c8"
+API_KEY = ""
+
+def set_api_key(api_key):
+    global API_KEY
+    API_KEY = api_key
+    
+def get_api_key():
+    return API_KEY
